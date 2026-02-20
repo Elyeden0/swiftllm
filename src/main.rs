d26523e09a4d36c340cfb1d00cd3598676c3b285
Alex <alex.bonnard2004@outlook.com>
2026-02-20 19:32:00 +0100

    init: scaffold rust project

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("llm-proxy starting...");
+    // TODO: add CLI argument parsing (--config, --port)
+    // TODO: load configuration from TOML file
+    // TODO: initialize HTTP server (axum?)
+    // TODO: set up provider routing
+}
